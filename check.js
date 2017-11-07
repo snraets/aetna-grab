6ee399a6320b8f6e3438e9ef131c4b7887023f9f
unknown <sjstearns@gmail.com>
2017-11-07 17:25:37 -0500

    First Results

--- check.js
+++ check.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const json = require('big-json');
 
-const readStream = fs.createReadStream('./results/results.20426.json');
+const readStream = fs.createReadStream('./results/results.20033.json');
 const parseStream = json.createParseStream();
 
 parseStream.on('data', (results) => {
