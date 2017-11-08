371be51f1f5dac5e86b91dcdb6b643fb49b5cd1c
checkbookjstearns <jstearns@checkbook.org>
2017-11-08 17:51:25 -0500

    Adding process

---
+++ process.js
@@ -0,0 +1,58 @@
+const {promisify} = require('util');
+const fs = require('fs');
+const ls = require('list-directory-contents');
+const json = require('big-json');
+const Rx = require('rxjs/Rx');
+
+const lsAsync = promisify(ls);
+
+(async () => {
+
+    const resultList =  await lsAsync('results');
+
+    Rx.Observable.from(resultList)
+        .concatMap( file => Rx.Observable.fromPromise(getJSON(file)))
+        .concatMap( results => Rx.Observable.from(results) )
+        .map( doctor => {
+
+            let parsedDoctor = {
+                id: doctor.id,
+                fullName: doctor.name,
+                first: doctor.name.match(/,\s\w*/) ? doctor.name.match(/,\s\w*/).pop() : '',
+                last: doctor.name.match(/^\w*(?=,)/) ? doctor.name.match(/^\w*(?=,)/).pop() : '',
+                address: doctor.address,
+                phone: doctor.telephone
+            };
+
+            parsedDoctor.specialty = doctor.properties1.filter( prop => !prop.includes('Years in Practice:') )
+                .map( prop => prop.replace('- Board Certified', '').trim())
+                .join(',');
+
+            return parsedDoctor;
+        })
+        .filter( doctor => doctor.fullName.includes('MD,'))
+        .subscribe({
+            next: doctor => void 0// console.log(doctor)
+        });
+    
+})()
+
+function getJSON(file){
+    
+    let p = new Promise((resolve, reject) => {
+
+        const readStream = fs.createReadStream(file);
+        const parseStream = json.createParseStream();
+
+        parseStream.on('data', results => {
+            resolve(results);
+        });
+        
+        readStream.pipe(parseStream);
+    });
+
+    return p;
+}
+
+
+
